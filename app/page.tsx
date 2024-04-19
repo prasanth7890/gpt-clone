b350568f0dfac9ed1efe1d6c6341d478c08b0325
prasanth7890 <maryratnamp@gmail.com>
2024-04-19 15:28:54 +0530

    fixed jwt token issues with jose

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,8 @@ export default function Home() {
   return (
     <div>
       <h2>GPT CLONE</h2>
-      <input
-        type="text"
-        placeholder="Enter your text"
-      />
-        <button>Submit</button>
+      <button>LOGIN</button>
+      <button>SIGNUP</button>
     </div>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,51 +1,48 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
+import * as jose from 'jose'
 
-
-
-export default async function middleware() {
-    console.log('hey');
+async function verify(token: string, secret: string) {
+    try {
+        const {payload} = await jose.jwtVerify(token, new TextEncoder().encode(secret));
+        return payload;
+    } catch (error: any) {
+        console.log(error.message);
+    }
 }
 
-
-
-
-
-// import { NextRequest, NextResponse } from 'next/server'
-// import { cookies } from 'next/headers'
-// import jwt from 'jsonwebtoken';
-
  
-// const protectedRoutes = ['/chat']
-// const publicRoutes = ['/signin', '/signup', '/']
+const protectedRoutes = ['/chat']
+const publicRoutes = ['/signin', '/signup']
  
-// export default async function middleware(req: NextRequest) {
-//   // 2. Check if the current route is protected or public
-//   const path = req.nextUrl.pathname
-//   const isProtectedRoute = protectedRoutes.includes(path)
-//   const isPublicRoute = publicRoutes.includes(path)
+export default async function middleware(req: NextRequest) {
+  // 2. Check if the current route is protected or public
+  const path = req.nextUrl.pathname
+  const isProtectedRoute = protectedRoutes.includes(path)
+  const isPublicRoute = publicRoutes.includes(path)
  
-//   // 3. Decrypt the session from the cookie
-//   const token = cookies().get('gpt-token')?.value || "";
-//   const session:any = jwt.verify(token, process.env.JWT_SECRET as string);
-//   console.log(session);
+  // 3. Decrypt the session from the cookie
+  const token = cookies().get('gpt-token')?.value || "";
+  const session = await verify(token, process.env.JWT_SECRET as string);
+  
+  // 5. Redirect to /login if the user is not authenticated
+  if (isProtectedRoute && !session?.email) {
+    return NextResponse.redirect(new URL('/signin', req.nextUrl))
+  }
  
-//   // 5. Redirect to /login if the user is not authenticated
-//   if (isProtectedRoute && !session?.email) {
-//     return NextResponse.redirect(new URL('/signin', req.nextUrl))
-//   }
+  // 6. Redirect to /chat if the user is authenticated
+  if (
+    isPublicRoute &&
+    session?.email &&
+    !req.nextUrl.pathname.startsWith('/chat')
+  ) {
+    return NextResponse.redirect(new URL('/chat', req.nextUrl))
+  }
  
-//   // 6. Redirect to /chat if the user is authenticated
-//   if (
-//     isPublicRoute &&
-//     session?.email &&
-//     !req.nextUrl.pathname.startsWith('/chat')
-//   ) {
-//     return NextResponse.redirect(new URL('/chat', req.nextUrl))
-//   }
+  return NextResponse.next()
+}
  
-//   return NextResponse.next()
-// }
- 
-// // Routes Middleware should not run on
-// export const config = {
-//   matcher: ['/chat']
-// }
+// Routes Middleware should not run on
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|/|favicon.ico).*)']
+}
