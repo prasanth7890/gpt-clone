1d31fa6e4c486f145ce2cb595d083e1130d8cc4d
prasanth7890 <maryratnamp@gmail.com>
2024-04-20 17:17:47 +0530

    added css to signin & signup pages

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 import { useSetRecoilState } from "recoil";
 import { userName } from "@/lib/store";
+import Link from "next/link";
 
 export default function SignIn() {
   const [email, setEmail] = useState<string>("");
@@ -17,31 +18,46 @@ export default function SignIn() {
       headers: {
         "Content-Type": "application/json",
       },
-      credentials: 'include',
-      body: JSON.stringify({email: email, password: password}),
+      credentials: "include",
+      body: JSON.stringify({ email: email, password: password }),
     });
 
     const res = await response.json();
-    if(res?.success) {
-      const username:string = email?.split('@')[0];
+    if (res?.success) {
+      const username: string = email?.split("@")[0];
       setUserName(username);
-      router.push('/chat');
+      router.push("/chat");
     }
   }
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="password"
-        onChange={(e) => setpassword(e.target.value)}
-      />
-      <button onClick={handlesubmit}>submit</button>
+    <div className="bg-[#131411] h-screen flex items-center justify-center text-white">
+      <div className="flex bg-[#27242c] flex-col h-[400px] w-[350px] items-center justify-center rounded-lg">
+        <h2 className="font-semibold text-2xl">Welcome back</h2>
+        <span className="text-xs pb-2 pt-1">Please enter your details</span>
+        <div className="flex flex-col p-1">
+          <label htmlFor="email" className="text-xs font-semibold m-1">Email</label>
+          <input
+            className="bg-[#131411] rounded-md p-1 pl-2 mb-2 placeholder:text-xs focus:outline-none"
+            name="email"
+            type="text"
+            placeholder="Enter your email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="password" className="text-xs font-semibold m-1">Password</label>
+          <input
+            className="bg-[#131411] rounded-md p-1 pl-2 mb-2 placeholder:text-xs focus:outline-none"
+            name="password"
+            type="text"
+            placeholder="............"
+            onChange={(e) => setpassword(e.target.value)}
+          />
+        <button onClick={handlesubmit} className="bg-blue-400 rounded-md p-2 mt-1 text-slate-50 w-[100%] text-xs">
+          Sign In
+        </button>
+        </div>
+        <span className="text-xs">Don't have an account? <Link href="/signup" className="text-blue-500">SignUp</Link></span>
+      </div>
     </div>
   );
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,35 +1,57 @@
 import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default async function Signup() {
-    async function handleSubmit(formData:FormData) {
-        'use server'
-        const email = formData.get('email');
-        const password = formData.get('password');
+  async function handleSubmit(formData: FormData) {
+    "use server";
+    const email = formData.get("email");
+    const password = formData.get("password");
 
-        const response = await fetch('http://localhost:5000/signup', {
-            method: 'POST',
-            body: JSON.stringify({email: email, password: password}),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
+    const response = await fetch("http://localhost:5000/signup", {
+      method: "POST",
+      body: JSON.stringify({ email: email, password: password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-        const {success, msg} = await response.json();
+    const { success, msg } = await response.json();
 
-        console.log(msg);
+    console.log(msg);
 
-        if(success) {
-            redirect('/signin');
-        }
+    if (success) {
+      redirect("/signin");
     }
+  }
 
-    return(
-        <div>
-            <form action={handleSubmit}>
-                <input type="text" placeholder="Email..." name="email"/>
-                <input type="text" placeholder="Password" name="password"/>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    )
+  return (
+    <div className="bg-[#131411] h-screen flex items-center justify-center text-white">
+      <div className="flex bg-[#27242c] flex-col h-[400px] w-[350px] items-center justify-center rounded-lg">
+        <h2 className="font-semibold text-2xl">Sign Up</h2>
+        <span className="text-xs pb-2 pt-1">Please enter your details</span>
+        <form action={handleSubmit} className="flex flex-col mt-1">
+          <label htmlFor="email" className="text-xs font-semibold m-1">
+            Email
+          </label>
+          <input
+            type="text"
+            placeholder="Email..."
+            name="email"
+            className="bg-[#131411] rounded-md p-1 pl-2 mb-2 placeholder:text-xs focus:outline-none"
+          />
+          <label htmlFor="password" className="text-xs font-semibold m-1">
+            Password
+          </label>
+          <input
+            type="text"
+            placeholder="Password"
+            name="password"
+            className="bg-[#131411] rounded-md p-1 pl-2 mb-2 placeholder:text-xs focus:outline-none"
+          />
+          <button type="submit" className="bg-blue-400 rounded-md p-2 mt-1 text-white w-[100%] text-xs">Sign Up</button>
+        </form>
+        <span className="text-xs m-1">Already had an account? <Link href="/signin" className="text-blue-500">SignIn</Link></span>
+      </div>
+    </div>
+  );
 }
